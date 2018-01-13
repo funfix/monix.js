b1a8c56ea33f75bb7e04c76410f1d6b84afef292
lostintime <lostintime.dev@gmail.com>
2018-01-13 08:54:40 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,67 @@
+# Monix.js
+
+Monix.js is a reactive streams library for JavaScript, [TypeScript](https://www.typescriptlang.org/) and [Flow](https://flow.org/).
+
+Inspired by [Scala](http://www.scala-lang.org/) and [Monix](https://monix.io/).
+
+## Usage
+
+The code is organized in sub-projects, for à la carte dependencies,
+but all types, classes and functions are exported by `monix`, so to
+import everything:
+
+```
+npm install --save monix
+```
+
+Or you can depend on individual sub-projects, see below.
+
+### Modules: UMD and ES 2015
+
+The library has been compiled using
+[UMD (Universal Module Definition)](https://github.com/umdjs/umd),
+so it should work with [CommonJS](http://requirejs.org/docs/commonjs.html)
+and [AMD](http://requirejs.org/docs/whyamd.html), for standalone usage
+in browsers or Node.js.
+
+But it also provides a `module` definition in `package.json`, thus
+providing compatibility with
+[ECMAScript 2015 modules](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import),
+for usage when used with a modern JS engine, or when bundling with a
+tool chain that understands ES2015 modules,
+like [Rollup](https://rollupjs.org/)
+or [Webpack](https://webpack.js.org/).
+
+## Sub-projects
+
+Monix.js has been split in multiple sub-projects for à la carte
+dependency management.  As mentioned above, you can depend on
+everything by depending on the `monix` project. 
+
+## TypeScript or Flow?
+
+Monix.js supports both [TypeScript](https://www.typescriptlang.org/)
+and [Flow](https://flow.org/) type annotations out of the box.
+
+It also makes the best use of the capabilities of each. For example
+TypeScript has bivariant generics, but Flow supports variance
+annotations and Monix.js makes use of them. Development happens in
+TypeScript, due to better tooling, but both are first class citizens.
+
+## Contributing
+
+The Monix.js project welcomes contributions from anybody wishing to
+participate.  All code or documentation that is provided must be
+licensed with the same license that Monix.js is licensed with (Apache
+2.0).
+
+Feel free to open an issue if you notice a bug, have an idea for a
+feature, or have a question about the code. Pull requests are also
+gladly accepted. For more information, check out the
+[contributor guide](CONTRIBUTING.md).
+
+## License
+
+All code in this repository is licensed under the Apache License,
+Version 2.0.  See [LICENCE](./LICENSE).
+
---
+++ scripts/update-common.js
@@ -0,0 +1,107 @@
+#!/usr/bin/env node
+/*!
+ * Copyright (c) 2018 by The Monix.js Project Developers.
+ * Some rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const process = require('process')
+const path = require('path')
+const fs = require('fs-extra')
+const { symlinkSync } = require('fs')
+
+const commonDir = path.join(path.dirname(process.argv[1]), "..", "common")
+const rootDir = path.join(commonDir, "..", "packages")
+
+const binariesToLink = [
+  "tsc",
+  "typedoc",
+  "ts-node",
+  "rollup",
+  "flow",
+  "tslint",
+  "mocha",
+  "nyc",
+  "rimraf"
+]
+
+const filesToLink = [
+  "tsconfig.json",
+  ".babelrc",
+  "rollup.config.js",
+  "tslint.json",
+  ".flowconfig",
+  ".editorconfig",
+  "mocha.opts",
+  "index.js.flow"
+]
+
+for (const p of fs.readdirSync(rootDir)) {
+  const dir = path.join(rootDir, p)
+  if (!fs.lstatSync(dir).isDirectory()) continue
+
+  for (const file of filesToLink) {
+    const target = path.join(dir, file)
+
+    if (!fs.existsSync(target)) {
+      const oldDir = process.cwd()
+      try {
+        process.chdir(dir)
+        const src = path.join("..", "..", "common", file)
+        console.info(`Creating symlink ${p}/${file} to ${src}`)
+        symlinkSync(src, target)
+      } finally {
+        process.chdir(oldDir)
+      }
+    }
+  }
+
+  for (const file of binariesToLink) {
+    const binDir = path.join(dir, "node_modules", ".bin")
+    const target = path.join(dir, "node_modules", ".bin", file)
+
+    if (!fs.existsSync(target)) {
+      const oldDir = process.cwd()
+      try {
+        fs.mkdirsSync(binDir)
+        process.chdir(binDir)
+
+        const src = path.join("..", "..", "..", "..", "node_modules", ".bin", file)
+        console.info(`Creating symlink ${p}/${file} to ${src}`)
+        symlinkSync(src, target)
+      } finally {
+        process.chdir(oldDir)
+      }
+    }
+  }
+
+  const dst = path.join(dir, "package.json")
+  const srcJSON = JSON.parse(fs.readFileSync(path.join(commonDir, "package.json"), "utf-8"))
+  const dstJSON = JSON.parse(fs.readFileSync(dst, "utf-8"))
+  for (const key of Object.keys(srcJSON)) dstJSON[key] = srcJSON[key]
+
+  // Changing dependencies, changing versions automatically!
+  if (srcJSON['version'] && dstJSON['dependencies']) {
+    for (const key of Object.keys(dstJSON['dependencies'])) {
+      if (key.match(/^monix/)) dstJSON['dependencies'][key] = srcJSON['version']
+    }
+  }
+
+  console.info(`Updating: ${dst}`)
+  fs.writeFileSync(
+    path.join(dir, "package.json"),
+    JSON.stringify(dstJSON, null, 2),
+    "utf-8"
+  )
+}
