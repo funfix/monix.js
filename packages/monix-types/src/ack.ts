0fc6ee1d1239f49c4d49aabbbdb86ebb72379cff
lostintime <lostintime.dev@gmail.com>
2018-02-17 14:08:47 +0200

    feat: add Ack types definition

--- packages/monix-types/src/ack.ts
+++ packages/monix-types/src/ack.ts
@@ -15,3 +15,44 @@
  * limitations under the License.
  */
 
+import { Future } from "funfix"
+
+/**
+ * Use `AckStop` to inform stream producer that it should stop sending values
+ */
+export type AckStop = "Stop"
+
+/**
+ * Single valid `AckStop` value, use it instead of hardcoding "Stop" strings
+ */
+export const Stop: AckStop = "Stop"
+
+/**
+ * Use `AckContinue` type value to inform stream producer to continue sending value
+ */
+export type AckContinue = "Continue"
+
+/**
+ * Single valid `Continue` value, is it instead of hardcoding "Contine" strings
+ */
+export const Continue: AckContinue = "Continue"
+
+/**
+ * Type alias for synchronous Ack values
+ */
+export type SyncAck = AckStop | AckContinue
+
+/**
+ * Type alias for asynchronous Ack values
+ */
+export type AsyncAck = Future<SyncAck>
+
+/**
+ * `Ack` type represents a set o values used to inform stream producer (source) about item consume status
+ *
+ * `SyncAck`: type alias for synchronous Ack values
+ *    - `Stop`: producer should stop sending values
+ *    - `Continue`: producer may continue sending values
+ * `AsyncAck`: asynchronous Ack type alias
+ */
+export type Ack = SyncAck | AsyncAck
